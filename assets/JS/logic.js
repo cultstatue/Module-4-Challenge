f9896e2ba3e98d2a3fafd2f4b220e452fb58e41c
cultstatue <saltedchurros@gmail.com>
2022-06-11 21:07:19 -0400

    some variables selected

---
+++ assets/JS/logic.js
@@ -0,0 +1,68 @@
+var quizContainer = document.querySelector("#quiz-container");
+var questionEl = document.querySelector("#question");
+
+var questions = [
+
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+        ]
+
+    },
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+        ]
+
+    },
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+        ]
+
+    },
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+        ]
+
+    },
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+        ]
+
+    }
+
+]
+
